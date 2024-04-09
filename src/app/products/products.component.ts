15e3fb66f076fe2737b2b8c3fcf1fc38c0c91c22
asseBadiane <badianeasse99@gmail.com>
2024-04-08 16:41:06 -1200

    🎉 display (App/products): displays products 💄

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products!: any[];
+  constructor() {}
 
+  ngOnInit(): void {
+    this.products = [
+      {
+        id: 1,
+        name: 'Computer',
+        price: 1300,
+      },
+      {
+        id: 2,
+        name: 'Printer',
+        price: 200,
+      },
+      {
+        id: 3,
+        name: 'Laptop',
+        price: 800,
+      },
+      {
+        id: 4,
+        name: 'Mobile',
+        price: 500,
+      },
+    ];
+  }
 }
