a7ad0bd4ca393702bdd5cb184b7e2da94edf4fb3
asseBadiane <badianeasse99@gmail.com>
2024-04-11 15:57:24 -1200

    ♻️feat(src/app):  implement Login form✨

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  userFormGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+  ngOnInit(){
+    this.userFormGroup = this.formBuilder.group({
+      username: this.formBuilder.control(''),
+      password: this.formBuilder.control('')
+    })
+  }
+
+  handleLogin(){
+    console.log(this.userFormGroup.value);
+  }
 
 }
