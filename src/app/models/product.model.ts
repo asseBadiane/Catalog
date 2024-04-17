87ecff5a09bfe617f7fc81ef6793dddb490ac225
asseBadiane <badianeasse99@gmail.com>
2024-04-17 16:35:02 +0000

    👔  Edit for better code

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,22 +1,14 @@
-
 export interface Product {
-    id: string;
-    name: string;
-    price: number;
-    promotion: boolean;
-}
-
-
-export interface Product_2 {
-    id: string;
-    name: string;
-    description: string;
-    price: number;
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  promotion: boolean;
 }
 
 export interface PageProduct {
-    products: Product[];
-    page: number;
-    size: number;
-    totalPages: number;
+  products: Product[];
+  page: number;
+  size: number;
+  totalPages: number;
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
-import { Product, Product_2 } from '../models/product.model';
+import { Product } from '../models/product.model';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
 import { Router } from '@angular/router';
@@ -11,44 +11,34 @@ import { Router } from '@angular/router';
   styleUrl: './products.component.css',
 })
 export class ProductsComponent implements OnInit {
-
   products!: Product[];
-  products_2!: Product_2[];
   errorMessage!: string;
   searchFormGroup!: FormGroup;
   currentPage: number = 0;
   pageSize: number = 5;
   totalPages: number = 0;
   currentAction: string = 'all';
- 
-  constructor(private productService: ProductService, private formBuilder: FormBuilder, public authService: AuthenticationService, private router: Router) {}
+
+  constructor(
+    private productService: ProductService,
+    private formBuilder: FormBuilder,
+    public authService: AuthenticationService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.searchFormGroup = this.formBuilder.group({
       keyword: this.formBuilder.control(''),
-    })
-    // this.handleGetAllProducts();
+    });
+    this.handleGetAllProducts();
     this.handleGetPageProducts();
-    this.handleGetAllProducts2();
   }
 
   handleGetAllProducts() {
     this.productService.getAllProducts().subscribe({
       next: (data) => {
         this.products = data;
-      },
-      error: (error) => {
-        this.errorMessage = error;
-        console.log(this.errorMessage);
-      },
-    });
-  }
-
-  handleGetAllProducts2() {
-    this.productService.getAllProducts2().subscribe({
-      next: (data) => {
-        this.products_2 = data;
-        console.log('Data', this.products_2);
+        console.log('Data', this.products);
       },
       error: (error) => {
         this.errorMessage = error;
@@ -58,36 +48,36 @@ export class ProductsComponent implements OnInit {
   }
 
   handleGetPageProducts() {
-    this.productService.getPageProducts(this.currentPage, this.pageSize).subscribe({
-      next: (data) => {
-        this.products = data.products;
-        this.totalPages = data.totalPages;
-      },
-      error: (error) => {
-        this.errorMessage = error;
-        console.log(this.errorMessage);
-      },
-    });
+    this.productService
+      .getPageProducts(this.currentPage, this.pageSize)
+      .subscribe({
+        next: (data) => {
+          this.products = data.products;
+          this.totalPages = data.totalPages;
+        },
+        error: (error) => {
+          this.errorMessage = error;
+          console.log(this.errorMessage);
+        },
+      });
   }
 
   goToPage(i: number) {
     this.currentPage = i;
     if (this.currentAction == 'all') {
       this.handleGetPageProducts();
-    }
-    else {
+    } else {
       this.handleSearchProducts();
-    } 
+    }
   }
 
   handleNewProduct() {
-    this.router.navigateByUrl("/admin/newProduct")
-    }
-  
+    this.router.navigateByUrl('/admin/newProduct');
+  }
+
   handleEditProduct(product: Product) {
-    this.router.navigateByUrl("/admin/editProduct/"+product.id)
-    }
-      
+    this.router.navigateByUrl('/admin/editProduct/' + product.id);
+  }
 
   handleDeleteProduct(product: Product) {
     let conf = confirm('Are you sure?');
@@ -117,19 +107,21 @@ export class ProductsComponent implements OnInit {
     });
   }
 
-  handleSearchProducts() { 
+  handleSearchProducts() {
     this.currentAction = 'search';
     this.currentPage = 0;
     let keyword = this.searchFormGroup.value.keyword;
-    this.productService.searchProducts(keyword, this.currentPage, this.pageSize).subscribe({
-      next: (data) => {
-        this.products = data.products;
-        this.totalPages = data.totalPages;
-      },
-      error: (error) => {
-        this.errorMessage = error;
-        console.log(this.errorMessage);
-      },
-    });
+    this.productService
+      .searchProducts(keyword, this.currentPage, this.pageSize)
+      .subscribe({
+        next: (data) => {
+          this.products = data.products;
+          this.totalPages = data.totalPages;
+        },
+        error: (error) => {
+          this.errorMessage = error;
+          console.log(this.errorMessage);
+        },
+      });
   }
 }
